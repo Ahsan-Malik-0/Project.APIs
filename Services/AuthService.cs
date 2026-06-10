fafdb0e54fa440bc4318632a311cb2cbd8ca68eb
Ahsan Malik <m.ahsan.malik951@gmail.com>
2026-06-10 15:36:55 +0500

    EditRoleFunctionality

--- Services/AuthService.cs
+++ Services/AuthService.cs
@@ -54,14 +54,14 @@ namespace Project.APIs.Services
 
                 if (usernameExists)
                 {
-                    if (memberDto.Role == "president")
-                    {
-                        throw new BusinessRuleException("President already exist");
-                    }
-                    else
-                    {
-                        throw new BusinessRuleException("Chairperson already exist");
-                    }
+                    //if (memberDto.Role.Contains("president", StringComparison.OrdinalIgnoreCase))
+                    //{
+                    throw new BusinessRuleException($"{memberDto.Role} already exist");
+                    //}
+                    //else
+                    //{
+                    //    throw new BusinessRuleException("Chairperson already exist");
+                    //}
                 }
             }
             else
