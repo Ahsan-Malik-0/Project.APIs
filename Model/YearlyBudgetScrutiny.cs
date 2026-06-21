e7456fbfa34b7a40e1ab13ee6d2eae69f3344d86
Ahsan Malik <m.ahsan.malik951@gmail.com>
2026-06-21 21:55:20 +0500

    CompleteAuditFunctionality

--- Model/YearlyBudgetScrutiny.cs
+++ Model/YearlyBudgetScrutiny.cs
@@ -5,10 +5,9 @@
         public Guid Id { get; set; }
         public required string Name { get; set; }
         public required string Comment { get; set; }
-        public string? Status { get; set; }
         public DateTime Date { get; set; }
-        public Guid AdministrationId { get; set; }
-        public Administration? Administration { get; set; }
+        public Guid MemberId { get; set; }
+        public Member? Member { get; set; }
         public Guid YearlyBudgetId { get; set; }
         public YearlyBudget? YearlyBudget { get; set; }
     }
